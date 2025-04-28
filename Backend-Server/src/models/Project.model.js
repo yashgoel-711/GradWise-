ad110ad71ca99743bc46582cc522865e0e78a4de
Your Name <you@example.com>
2025-04-28 12:47:44 +0530

    fix: Update error messages in GetInfoProjectDetails and GetUpdateProjectDetails methods for clarity

--- Backend-Server/src/models/Project.model.js
+++ Backend-Server/src/models/Project.model.js
@@ -26,11 +26,45 @@ const projectSchema = new mongoose.Schema({
     }
 
   }, { timestamps: true });
+
   projectSchema.methods.GetInfoProjectDetails = async function(id){
     if(!id){
-      throw new apiError(400,"give the  id of the project details")
+      throw new apiError(400,"give the  id of the student  project details") 
+    }
+    try {
+      if(this.uploadedBy.toString() === id.toString()){
+        return {"githubRepo":this.githubRepo,"status":this.status,"summary":this.summary,"techStack":this.techStack,"domain":this.domain}
+      }
+      else{
+        throw new apiError(403,"you are not allowed to see the project details because the id is not same")
+      }
+    } catch (error) {
+      throw new apiError(500,"error in getting the project details")
+      
     }
   }
-   
+  projectSchema.methods.GetUpdateProjectDetails = async function(id,body){
+    if(!id){
+      throw new apiError(400,"give the  id of the student  project details") 
+    }
+    try {
+      if(this.uploadedBy.toString() === id.toString()){
+        this.githubRepo = body.githubRepo || this.githubRepo;
+        this.status = body.status || this.status;
+        this.summary = body.summary || this.summary;
+        this.techStack = body.techStack || this.techStack;
+        this.domain = body.domain || this.domain;
+        await this.save()
+        return {"githubRepo":this.githubRepo,"status":this.status,"summary":this.summary,"techStack":this.techStack,"domain":this.domain}
+      }
+      else{
+        throw new apiError(403,"you are not allowed to see the project details because the id is not same")
+      }
+    } catch (error) {
+      throw new apiError(500,"error in getting the project details")
+      
+    }
+  }
+  
 
 export const Project = mongoose.model("Project",projectSchema)
